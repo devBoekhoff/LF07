457d46de1cf27c4f0d77a0a8685e3d55d9726410
devBoekhoff <dev.boekhoff@gmail.com>
2022-05-31 12:35:04 +0200

    test code for servo

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 80;
-const repository = require("./modules/repository")
+const repository = require("./modules/repository");
+const piServo = require('pi-servo');
 
 app.use(express.static("public"));
 app.use(express.static("shared"));
@@ -21,3 +22,20 @@ app.listen(port, () => {
 });
 
 setInterval(repository.save, 5000);
+
+const servo = new piServo(22);
+
+async function moveServo(){
+    try{
+        await servo.open();
+    } catch{
+        console.log("couldn't open sensor");
+        return;
+    }
+    await servo.setDegree(90);
+    await sleep(1000);
+    await servo.open();
+    await servo.setDegree(0);
+}
+
+setInterval(moveServo, 2000);
